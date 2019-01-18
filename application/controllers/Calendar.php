03c607bf71d1e4700d012b68021a70858985773a
horrorchix89 <44495469+horrorchix89@users.noreply.github.com>
2019-01-18 02:06:41 -0600

    Update Calendar.php

--- application/controllers/Calendar.php
+++ application/controllers/Calendar.php
@@ -10,7 +10,7 @@ class Calendar extends CI_Controller
 
     public function index() 
     {
-        $this->load->view("index.html", array());
+        $this->load->view("calendar/index.php", array());
     }
 
     public function get_events() 
